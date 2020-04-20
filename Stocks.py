b1e0765d825ed58ed4bc06f0617b99fe7ba2918f
Dekevion <dekevionwade@yahoo.com>
2020-04-19 22:49:06 -0500

    '..'

--- Stocks.py
+++ Stocks.py
@@ -1,3 +1,5 @@
+import math
+# import round
 # Program that calculates -3% of a number and +10% of a number
 # example: number is 100 and answer is 97 and 110
 # gain = float(0.1)
@@ -9,6 +11,7 @@
 # toloss = loss * buy
 
 # print(tobuy)
+
 # print(toloss)
 
 def Stocks():
@@ -16,8 +19,11 @@ def Stocks():
     my_gain = 10 / 100 * my_number
     my_loss = -3 / 100 * my_number
 
+    number_gain = round(my_number + my_gain, 2)
+    number_loss = round(my_number + my_loss, 2)
+   
 
     # print(f'Your gain from the number {my_number} is  +{my_gain}, and your loss is -{my_loss}')
-    print(f'you gained {my_number + my_gain} and you lost {my_number + my_loss}')
+    print(f'you gained {number_gain} and you lost {number_loss}')
 
 Stocks()
